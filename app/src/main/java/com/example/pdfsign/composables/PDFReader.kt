e8f695ae3712292080ae524b39ddf781e71d38e0
regxl <rathourabhishekvk4@gmail.com>
2024-05-02 00:47:06 +0530

    added some changes in PDFReader

--- app/src/main/java/com/example/pdfsign/composables/PDFReader.kt
+++ app/src/main/java/com/example/pdfsign/composables/PDFReader.kt
@@ -1,6 +1,5 @@
 package com.example.pdfsign.composables
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -9,35 +8,19 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
 import com.example.pdfsign.utils.PdfRender
 import com.example.pdfsign.viewModels.ImageInfo
-import com.example.pdfsign.viewModels.SharedViewModel
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -45,36 +28,45 @@ fun PDFReader(
     pdfRender: PdfRender,
     navigateToPdfPageEdit: (ImageInfo) -> Unit
 ) {
-    val pagerState = rememberPagerState {
-        pdfRender.pageCount
-    }
-    HorizontalPager(state = pagerState) { index ->
-        Box(contentAlignment = Alignment.Center){
-            val page = pdfRender.pageLists[index]
-            DisposableEffect(Unit) {
-                page.load()
-                onDispose {
-                    page.recycle()
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val pagerState = rememberPagerState {
+            pdfRender.pageCount
+        }
+        HorizontalPager(state = pagerState, modifier = Modifier.fillMaxSize()) { index ->
+            BoxWithConstraints {
+                val page = pdfRender.pageLists[index]
+                DisposableEffect(Unit){
+                    page.load()
+                    onDispose {
+                        page.recycle()
+                    }
                 }
-            }
-            page.pageContent.collectAsState().value?.asImageBitmap()?.let { imageBitmap ->
-                Image(
-                    bitmap = imageBitmap,
-                    contentDescription = "Pdf page number: $index",
+                page.pageContent.collectAsState().value?.asImageBitmap()?.let { imageBitmap ->
+                    Image(
+                        bitmap = imageBitmap,
+                        contentDescription = "Pdf page number: $index",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .align(Alignment.Center)
+                            .pointerInput(Unit) {
+                                detectTapGestures(onDoubleTap = {
+                                    navigateToPdfPageEdit(ImageInfo(imageBitmap = imageBitmap, index = index))
+                                })
+                            },
+                        contentScale = ContentScale.Fit
+                    )
+                } ?: Box(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .align(Alignment.Center)
-                        .pointerInput(Unit) {
-                            detectTapGestures(onDoubleTap = {
-                                navigateToPdfPageEdit(
-                                    ImageInfo(
-                                        imageBitmap = imageBitmap,
-                                        index = index
-                                    )
-                                )
-                            })
-                        },
-                    contentScale = ContentScale.Fit
+                        .fillMaxWidth()
+                        .height(
+                            with(LocalDensity.current) {
+                                page
+                                    .heightByWidth(constraints.maxWidth)
+                                    .toDp()
+                            }
+                        )
                 )
             }
         }
